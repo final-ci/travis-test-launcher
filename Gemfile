ba0a91fe455e5c23098d00ef58c424a139b3dd1a
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-05-07 10:50:55 +0200

    feat: revrite build_runner to support stash

---
+++ Gemfile
@@ -0,0 +1,19 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+ruby '2.1.5'
+
+
+#gem 'travis-core',   github: 'final-ci/travis-core'
+gem 'travis-core',   path: '../travis-core'
+gem 'travis-support',     github: 'final-ci/travis-support'
+gem 'travis-config',   '~> 0.1.0'
+gem 'travis-sidekiqs',    github: 'final-ci/travis-sidekiqs', require: nil
+
+gem 'connection_pool'
+gem 'activesupport'
+gem 'diff-lcs'
+
+gem 'pg'
+
+# gem "rails"
---
+++ bin/build_runner.rb
@@ -0,0 +1,109 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'bundler/setup'
+require 'travis'
+require 'json'
+
+options = {
+  event_type: 'stash_push'
+}
+help = nil
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: Builds executor"
+  help = opts
+
+  opts.on(
+    "-p",
+    "--payload PAYLOAD",
+    "Payload containing push request, use prefix @ for payload stored in file"
+  ) do |p|
+    options[:payload] = p
+  end
+
+  opts.on(
+    "-g",
+    "--git-provider-event [GIT_PROVIDER_EVENT]",
+    "Git provider, should be stash_push (default) or push, pull, api for github"
+  ) do |g|
+    options[:event_type] = g
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do |h|
+    puts opts
+    exit
+  end
+end.parse!
+
+unless options[:payload]
+  puts "Wrong aruments:\n\n";
+  puts help
+  exit
+end
+
+if options[:payload][0] == '@'
+  payload_file = options[:payload][1..-1]
+  options[:payload] = File.read(payload_file)
+end
+
+
+Travis::Database.connect
+
+payload = JSON.parse(options[:payload]).to_hash
+
+user_name = payload['repository']['slug'].split('/').first
+repo_name = payload['repository']['slug'].split('/').last
+
+#payload.deep_merge!({
+#  'repository' => {
+#    'owner' => {
+#      'name' => user_name
+#    }
+#  }
+#});
+
+#add some specific fields
+queue_payload = {
+  event_type: 'stash_push',
+  credentials: {
+    'login' => user_name
+  },
+  payload: payload,
+  uuid: Travis.uuid
+}
+
+#search whether the specified user exists
+user = User.where(name: user_name).first
+user = User.create!(
+  name: user_name,
+  login: user_name,
+  github_id: Time.now.to_i,
+  is_admin: false,
+  is_syncing: true) unless user
+
+#search whether the specified repo exists
+repository = Repository.where(
+  name: payload['repository']['name'],
+  url: payload['repository']['url']
+).first
+
+repository = Repository.create!(
+  name: payload['repository']['name'],
+  provider: 'stash',
+  owner: user,
+  owner_name: user.name,
+  active: true,
+  url: payload['repository']['url'],
+  private: false) unless repository
+
+queue_payload[:payload]['repository']['repository_id'] = repository.id
+
+#run the service
+request = Travis.service(:receive_request, user, queue_payload).run
+
+puts
+puts request.inspect
+
+exit(request ? 0 : 1)
+
